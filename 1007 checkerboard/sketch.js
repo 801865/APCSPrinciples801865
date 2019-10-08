806cd0473668c20329462d8333dd3cc29392dd73
iMac <imac@imacs-imac.attlocal.net>
2019-10-07 17:01:59 -0700

    checker board

--- 1007 checkerboard/sketch.js
+++ 1007 checkerboard/sketch.js
@@ -6,24 +6,27 @@ Danny Ramirez
 
 //  Global variables
 // Declare squares[] as a global variable
-var squares = []
-
+var square = []
 // put setup code here
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(235);
+  loadSquares(1);
   // Create a function that loads the squares into an
   // array of square objects and call the function
-  loadSquares(2); //  traverse array and run each square
+  loadSquares(); //  traverse array and run each square
   for(var i = 0; i < square.length; i++){
-    square[i].render();
+    square[i].run();
   }
 }
 
 
 
-function loadSquares(){
+function loadSquares(n){
+  for(var i = 0; i < n; i++){
+    square[i] = new Square(0, 0, 100, 100);
+  }
   //  create a square object and push it into the squares array
   //  create a loop that will push 64 squares into an array
   //  calculate the x and y position interms of the loop control variable
--- 1007 checkerboard/square.js
+++ 1007 checkerboard/square.js
@@ -4,19 +4,38 @@
 **  Sept. 11, 2018
 */
 //  Add perameters to your constructor function
-function Sqr(){
+class Square{
+  constructor(x, y, w, h){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = w;
+    this.clr1 = color(random(255), random(255), random(255));
+    this.clr2 = color(random(255), random(255), random(255));
+  }
 // Add instance variables to your constructor Function
 //What does a square need to know about itself??
-constructor(x, y, w, h){
-    this.x = 0;
-    this.y = 0;
-    this.w = 20;
-    this.h = 20;
-    this.clr = color(random(255), random(255), random(255))
-}
-
-  this.render = function(){
-    rect(this.x, this.y, this.w, this.h);
+  run(){
+    this.render();
   }
+    render(){
+      fill(this.clr2);
+      rect(this.x, this.y, this.w, this.h);
+      var j = 0;
+      for(var s = 0; s <= 8; s++){
+        var add = 101;
+        var add2 = 101;
+        for(j = j; j <= 8; j++){
+          rect(this.x + add, this.y, this.w, this.h);
+          add = add + 101;
+          if(j % 2 === 0){
+            fill(this.clr1);
+          }else{
+            fill(this.clr2);
+          }
+        }
+        rect(this.x, this.y + add2, this.w, this.h);
+      }
+    }
 
 }//  End of Square constructor function
