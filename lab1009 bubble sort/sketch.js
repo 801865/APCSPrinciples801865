b4509208133f29d77419474f307bae70d12014bd
801865 <54591277+801865@users.noreply.github.com>
2019-10-10 08:50:25 -0700

    prep

--- lab1009 bubble sort/sketch.js
+++ lab1009 bubble sort/sketch.js
@@ -2,17 +2,22 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
+var list = [];
+var j = 1;
+var temp = 0;
+var i = 4;
+
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  var list = [4, 5, 7, 8];
+  list = [11, 5, 4, 1];
   function bubbleSort(){
-    for (var i = list.length; i >= 0; i--){
-      for(var j = 1; j <= i; i++){
-        if(list[j - 1]) > list[j]){
-          var temp = list[j - 1];
+    for (i = list.length; i >= 0; i--){
+      for(j = 1; j <= i; i++){
+        if(list[j - 1] > list[j]){
+          temp = list[j - 1];
           list[j - 1] = list[j];
           list[j] = temp;
         }// check swap
@@ -24,5 +29,4 @@ function setup() {
 
 //  The draw function is called @ 30 fps
 function draw() {
-  console.log(list);
 }
