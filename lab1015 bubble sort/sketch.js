f12f8e27c09d92c11ad9838a0ec6faafd0a2d6fa
801865 <54591277+801865@users.noreply.github.com>
2019-10-15 08:49:45 -0700

    bubble sort

--- lab1015 bubble sort/sketch.js
+++ lab1015 bubble sort/sketch.js
@@ -3,22 +3,31 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 var list = [];
+var i = 0;
+var j = 0;
+var temp = 0
 
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  list = [11, 5, 4, 1];
+  for(a = 0; a <= 30; a++){
+    list = list + random(0, 30);
+  }
+  //list = [11, 5, 4, 1, 20, 30, 500];
+  console.log(list);
+  bubbleSort();
+  console.log(list);
 }
 
 function bubbleSort() {
-  for(var i = list.length; i >= 0; i--){
-    for(var j = 1; j <= i; i++){
-      if(list[j - 1] < list[j]){
-        var temp = list[j - 1];
-        list[j - 1] = list[j];
-        list[j] = temp;
+  for(i = 0; i < list.length - 1; i++){
+    for(j = 0; j < list.length - 1 - i; j++){
+      if(list[j + 1] < list[j]){
+        temp = list[j];
+        list[j] = list[j + 1];
+        list[j + 1] = temp;
       }// check swap
     }//end of second loop
   }//end of first loop
