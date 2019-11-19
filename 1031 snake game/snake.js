32fd0e1c31690f9d3fa7b96af7c07ac7bfad069b
801865 <54591277+801865@users.noreply.github.com>
2019-11-19 08:50:55 -0800

    snnakke

--- 1031 snake game/snake.js
+++ 1031 snake game/snake.js
@@ -19,9 +19,14 @@ class Snake{
   update(){
     var x = this.head.x;
     var y = this.head.y;
-    this.segments = [] + j;
-    this.segments[j - 1].x = x;
-    this.segments[j - 1].y = y;
+    for(j = this.segments.length - 1; j > 0; j--){
+      this.segments[j].x = this.segments[j - 1].x;
+      this.segments[j].y = this.segments[j - 1].y;
+    }
+    if(this.segments.length > 0){
+      this.segments[0].x = x;
+      this.segments[0].y = y;
+    }
     if(x > 0 && x < width && y > 0 && y < height){
       this.head.add(this.vel);
     }else{
@@ -29,13 +34,20 @@ class Snake{
        this.vel.y = 0;
        gameState = 3;
     }//checks if snake is off the screen
+    if(this.segments.length > 0){
+      for(j = 0; j <= this.segments.length; j++){
+        if(x === this.segments[j].x && y === this.segments[j].y){
+          gameState = 3;
+        }//checks if snake crashes with itself
+      }//end of for loop
+    }//end of first if statement
   }//end of update
 
   render(){
     fill(0, 255, 0);
     rect(this.head.x, this.head.y, this.w, this.h);
     for(var i = 0; i < this.segments.length; i++){
-      rect(this.segments.x, this.segments.y, this.w, this.h);
+      rect(this.segments[i].x, this.segments[i].y, this.w, this.h);
     }//creates new segment
   }//end of render
 }//end of snake class
